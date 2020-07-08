6b4bb253f052e880b640808ba86b4cc0eb5820cc
Captain-TNT <teghjyot@icloud.com>
2020-07-08 23:36:30 +1000

    Refined some bits of code

--- script.js
+++ script.js
@@ -1,41 +1,28 @@
-/*const themeMap = {
-	dark: "light",
-	light: "dark",
-};
-
-const theme =
-	localStorage.getItem("theme") ||
-	((tmp = Object.keys(themeMap)[0]), localStorage.setItem("theme", tmp), tmp);
-const bodyClass = document.body.classList;
-bodyClass.add(theme);
-
-function toggleTheme() {
-	const current = localStorage.getItem("theme");
-	const next = themeMap[current];
-
-	bodyClass.replace(current, next);
-	localStorage.setItem("theme", next);
-
-}
-
-document.getElementById("themeButton").onclick = toggleTheme;
-*/
 var t = false;
+
 function toggleIt(x) {
 	if (t) {
+		//dark
 		document.getElementById("toggleOff").classList.remove("fa-toggle-off");
 		document.getElementById("toggleOff").classList.add("fa-toggle-on");
+		document.getElementById("houseTime").classList.remove("fa-house-day");
+		document.getElementById("houseTime").classList.add("fa-house-night");
 		document.getElementById("toggleOff").style =
 			"--fa-primary-color: rgb(0, 122, 255); --fa-secondary-color: rgb(255, 255, 255);";
-		document.body.classList.remove("dark");
-		document.body.classList.add("light");
-	} else {
-		document.getElementById("toggleOff").classList.remove("fa-toggle-on");
-		document.getElementById("toggleOff").classList.add("fa-toggle-off");
-		document.getElementById("toggleOff").style =
-			"--fa-primary-color: rgb(255, 255, 255);";
 		document.body.classList.remove("light");
 		document.body.classList.add("dark");
+	} else {
+		//light
+		document.getElementById("toggleOff").classList.remove("fa-toggle-on");
+		document.getElementById("toggleOff").classList.add("fa-toggle-off");
+		document.getElementById("houseTime").classList.remove("fa-house-night");
+		document.getElementById("houseTime").classList.add("fa-house-day");
+		document.getElementById("toggleOff").style =
+			"--fa-primary-color: rgb(255, 255, 255);";
+		document.body.classList.remove("dark");
+		document.body.classList.add("light");
 	}
 	t = !t;
 }
+
+toggleIt();
