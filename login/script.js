bf28daad0f8c9ce773df3449f01ad12142a3b136
Teghjyot <66196010+Teghjyot@users.noreply.github.com>
2021-01-26 00:37:59 +1100

    added logout function

--- login/script.js
+++ login/script.js
@@ -1,13 +1,23 @@
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 // const loginErrorMsg = document.getElementById("login-error-msg");
-
+var loggedIn = localStorage.getItem("loggedIn");
+if (!loggedIn){
+  loggedIn = false;
+  console.log("LOGFALSE");
+} else if (loggedIn) {
+  loggedIn = true;
+  console.log("LOGTRUE");
+  window.location.replace('../profile/');
+} else {
+  console.log(localStorage.getItem("loggedIn"))
+}
 var db;
 
 fetch('./data.json')
   .then(res => res.json())
   .then(data => {
-    db = data
+    db = data;
   })
 
 loginButton.addEventListener("click", (e) => {
@@ -15,24 +25,34 @@ loginButton.addEventListener("click", (e) => {
   const username = loginForm.querySelector('.username').value;
   const password = loginForm.querySelector('.password').value;
 
-  var loggedIn = false;
 
-  for (let i = 0; i < db.loginDetails.length; i++) {
-    if (db.loginDetails[i].includes("♩")) {
-      var splitArr = db.loginDetails[i].split("♩");
-      if (username == splitArr[0] && password == splitArr[1]) {
-        console.log('Successfully Logged in.');
-        loggedIn = true;
-        return;
-      }
+  
+  var DUsername = db[username.toString()];
+
+  checkUser = (username) => {
+    return (username ? username : false);
+  }
+
+  checkPssd = (password, username) => {
+    return (password == username[0]["password"]) ? true: false;
+  }
+
+  var usr = checkUser(DUsername);
+  if (usr) {
+    var pssd = checkPssd(password, usr);
+    if (pssd) {
+      console.log(pssd);
+      loggedIn = true;
+      localStorage.setItem("loggedIn", loggedIn);
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+      window.location.replace('../profile/');
+    } else {
+      loggedIn = false;
     }
+  } else {
+    loggedIn = false;
   }
 
-  if (!loggedIn) {
-    console.log("KILL ME");
-  }
-  
-  
 
-    
 })
